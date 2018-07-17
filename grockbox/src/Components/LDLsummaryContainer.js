2009e4dd553bf408f27c3977d725f352430e9469
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-07-17 09:55:18 -0400

    imports and exports inserted

---
+++ grockbox/src/Components/LDLsummaryContainer.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import LDLsummaryLeft from './Component/LDLsummaryLeft';
+import LDLsummaryRight from './Component/LDLsummaryRight';
+
+
+
+export default LDLsummaryContainer
---
+++ grockbox/src/Components/LDLsummaryRight.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import LDLsummaryRightcomponent from './Component/LDLsummaryRightcomponent';
+
+
+
+
+
+export default LDLsummaryRight

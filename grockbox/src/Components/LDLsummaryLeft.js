bad2f2923dea0e6479afb1fff0f6dd4fb607b965
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-07-19 08:33:08 -0400

    inserted sketch image

--- grockbox/src/Components/LDLsummaryLeft.js
+++ grockbox/src/Components/LDLsummaryLeft.js
@@ -4,14 +4,25 @@ import React, {Component} from 'react';
 
 const LDLsummaryLeft = props =>{
     return(
-        <div>
+        <div className = "entireLeftSide">
         <div className = "image">
-        <h1>"PIC HERE"</h1>
+
+
+        <svg width="80px" height="43px" viewBox="0 0 80 43" version="1.1" >
+
+            <desc>Created with Sketch</desc>
+            <defs></defs>
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="square">
+                <g id="weirdColorTabs" transform="translate(-59.000000, -36.000000)" fill-rule="nonzero" stroke="#A04040" stroke-width="10">
+                    <path d="M64.8428247,73.5 C64.8428247,65.3508873 71.0134452,54.9811895 74.8650404,49.9795567 C82.189566,40.4680203 94.8954645,41.5888067 99.2817752,41.5888067 C104.452599,41.5888067 117.518515,42.8990116 123.102541,49.9795567 C130.387919,59.2174178 133.720726,68.8755057 133.720726,73.5" id="Line"></path>
+                </g>
+            </g>
+        </svg>
         </div>
         <p className = "P1">03/18/2018 - Rosavastatin 20mg</p>
-        <p className = "P2">LDL GOAL: <span>&lt;70</span></p>
-        <p className = "P3"> DM-1, HeFH, ASCVD | <span>RISK SCORE</span>: 7</p>
-        <p className = "P4"> <span>STATIN INTENSITY:</span>[HIGH]</p>
+        <p className = "P2">LDL GOAL: <span> &lt; 70</span></p>
+        <p className = "P3"> DM-1, HeFH, ASCVD  |  <span className="risk">RISK SCORE</span>: 7</p>
+        <p className = "P4"> <span className="intensity">STATIN INTENSITY:</span> [HIGH]</p>
         </div>
     )
 }
--- grockbox/src/Components/LDLsummaryRightcomponent.js
+++ grockbox/src/Components/LDLsummaryRightcomponent.js
@@ -3,8 +3,13 @@ import React, {Component} from 'react';
 const LDLsummaryRightcomponent = props =>{
     return(
         <div className= "statsGroup">
-        <p><span>{props.ldlDescrip}</span>{props.number}{props.date}</p>
-        <p>{props.name}</p>
+        <div className="rightDivs">
+        <div className = "ldlDescrip">{props.ldlDescrip}</div>
+        <div className= "number">{props.number}</div>
+        <div className = "date">{props.date}</div>
+        </div>
+        <p className = "name">{props.name}</p>
+
         </div>
     )
 
